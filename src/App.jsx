c03daaa0ad65b8c072142210f9c9de9a53216d03
codezeros18 <125339231+codezeros18@users.noreply.github.com>
2025-06-12 02:26:59 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,23 @@ import Temple from './pages/Temple';
 import Mountain from './pages/Mountain';
 import Dungeon from './pages/Dungeon';
 import { GameProvider } from "./GameContext"; // ✅
+import bgm from './assets/song/music.mp3'; // adjust path if needed
+import React, { useRef, useEffect } from 'react';
 
 
 function App() {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.5;
+    }
+  }, []);
+
   return (
     <GameProvider>
+      {/* Infinite background music */}
+      <audio ref={audioRef} src={bgm} autoPlay loop />
       <Router>
         <Routes>
           <Route path="/" element={<Character />} />
